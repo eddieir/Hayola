8c8901cad586fbcef24a2675540624968f0b0c36
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2019-11-04 17:21:27 +0100

    update - add the shell commandas

---
+++ scripts/getPredifinedModel.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+mkdir ../bin
+wget http://sketch-code.s3.amazonaws.com/model_json_weights/model_json.json -O ../bin/model_json.json
+wget http://sketch-code.s3.amazonaws.com/model_json_weights/weights.h5 -O ../bin/weights.h5
